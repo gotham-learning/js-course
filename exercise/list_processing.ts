0e7163b502fb07c0e0ceee47470d7207f8be1e98
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-07-03 11:11:14 +0700

    Add exercise on string formating

--- exercise/list_processing.ts
+++ exercise/list_processing.ts
@@ -3,26 +3,30 @@ type Game = { name: string, rating: number }
 // 1. returns list of name from list of game
 const names = (games: Game[]): string[] => []
 
-// 2. get sum of all the ratings in list of game
+// 2. returns list of string that has format `<name> [<rating>]` 
+// eg. `call of duty [7.4]`
+const formatGames = (games: Game[]): string[] => []
+
+// 3. get sum of all the ratings in list of game
 const totalRatingsOf = (games: Game[]): number => 0
 
-// 3. get average of all the ratings in list of game
+// 4. get average of all the ratings in list of game
 const averageRatingsOf = (games: Game[]): number => 0
 
-// 4. get total rating by name from list of game
+// 5. get total rating by name from list of game
 const totalRatingsByName = (name: string, games: Game[]): number => 0
 
-// 5. get total rating by name from list of game
+// 6. get total rating by name from list of game
 const averageRatingsByName = (name: string, games: Game[]): number => 0
 
-// 6. increase rating for all games
+// 7. increase rating for all games
 const increaseRatingBy = (inc: number, games: Game[]): Game[] => []
 
-// 7. decrease rating for game with specific name
+// 8. decrease rating for game with specific name
 const decreaseRatingBy = (dec: number, name: string, games: Game[]): Game[] => []
 
-// 8. returns game if the name exist else returns undefined
+// 9. returns game if the name exist else returns undefined
 const findByName = (name: string, games: Game[]): Game[] => []
 
-// 9. decrease rating for game that has name in the list
+// 10. decrease rating for game that has name in the list
 const decreaseRatingForNames = (dec: number, names: string[], games: Game[]): Game[] => []
